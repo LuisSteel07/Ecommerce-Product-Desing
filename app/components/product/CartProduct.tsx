4a2a7dbda28029355728c6d5ddc39914609f0591
LuisSteel07 <luisdelfonseca220@gmail.com>
2025-07-06 11:07:55 -0400

    Resolved width and height error un CartProduct component

--- app/components/product/CartProduct.tsx
+++ app/components/product/CartProduct.tsx
@@ -10,12 +10,12 @@ type CartProductProps = {
 export default function CartProduct({product, count, setCount}: CartProductProps){
     return (
         <section className="flex flex-row gap-4 items-center text-black/60">
-            <Image src={product.list_images[0]} alt="product image" className="w-16 rounded-md" />
+            <Image src={product.list_images[0]} alt="product image" width={64} height={64} className="w-16 rounded-md" />
             <div className="flex flex-col">
                 <p>{product.title}</p>
                 <p>${(product.price * product.discount) / 100} x {count} <span className="font-bold text-black">${((product.price * product.discount) / 100) * count}</span></p>
             </div>
-            <Image src="/icon-delete.svg" alt="icon delete" className="w-4 h-4" onClick={() => setCount(0)} />
+            <Image src="/icon-delete.svg" alt="icon delete" width={16} height={16}  className="w-4 h-4" onClick={() => setCount(0)} />
         </section>
     )
 }
